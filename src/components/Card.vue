55f9518198ca4fb048067b4bd47cb116efbdab51
Denys <denys1984.25@gmail.com>
2024-03-17 10:11:14 -0600

    Main-page: Created basic main page and card component

---
+++ src/components/Card.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+defineProps({
+  price: Number,
+  title: String,
+  imageUrl: String,
+  isFavorite: Boolean,
+  isAdded: Boolean,
+  onClickAdd: Function,
+  onClickFavorite: Function,
+})
+</script>
+
+<template>
+  <div
+    class="relative bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl"
+  >
+    <img 
+      :src="isFavorite ? '/like-1.svg' : '/like-2.svg'" 
+      alt="Favorite" 
+      class="absolute top-8 left-8" 
+      @click="onClickFavorite"
+    />
+    <img :src="imageUrl" alt="Sneaker" />
+    <p class="mt-2">
+      {{ title }}
+    </p>
+
+    <div class="flex justify-between mt-5">
+      <div class="flex flex-col gap-2">
+        <span class="text-slate-400">
+          {{ $t('card.price') }}:
+        </span>
+        <span class="font-bold">{{ price }} usd</span>
+      </div>
+      <img 
+        :src="isAdded ? '/checked.svg' : '/plus.svg'" 
+        alt="Plus" 
+        @click="onClickAdd" 
+      />
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/plugins/i18n/locales/en/index.js
@@ -0,0 +1,9 @@
+import header from "./header"
+import card from "./card"
+import main_page from "./main-page"
+
+export const EN_LOCALE = {
+  header,
+  card,
+  main_page,
+}
