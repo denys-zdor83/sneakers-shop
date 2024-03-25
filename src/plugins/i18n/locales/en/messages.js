38c2c1a88c125d6c8e425f5df39962ecb1b12b01
Denys <denys1984.25@gmail.com>
2024-03-24 22:03:08 -0600

    Refactoring: Added message block about empty favorites

--- src/plugins/i18n/locales/en/messages.js
+++ src/plugins/i18n/locales/en/messages.js
@@ -6,5 +6,9 @@ export default {
   order_sent: {
     title: "Your order #{orderId} has been sent",
     description: 'We will contact you as soon as possible',
-  }
+  },
+  empty_favorites: {
+    title: 'There is nothing yet here',
+    description: 'Choose some products to add to your favorites',
+  },
 }
